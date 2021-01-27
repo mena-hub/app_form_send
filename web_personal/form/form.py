4c537b368d00962258930949cdb4ce6e3fa578ef
mena-hub <mssz.nnia@gmail.com>
2021-01-27 12:30:13 -0300

    Delete form.py

--- web_personal/form/form.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Emails 
-
-class EmailsForm(ModelForm):
-    
-    class Meta:
-        
-        model = Emails
-        fields = ['email', 'subject', 'message']
