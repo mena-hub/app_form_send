a321a195284a85d1257bc8df2980f47f0d48f501
mena-hub <mssz.nnia@gmail.com>
2021-01-27 13:26:19 -0300

    Undo (again) and run

--- web_personal/form/admin.py
+++ web_personal/form/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-from django.contrib import admin
-from .models import Emails
-
-class EmailsAdmin(admin.ModelAdmin):
-    list_display = ('email', 'subject', 'message')
-
-admin.site.register(Emails, EmailsAdmin)
