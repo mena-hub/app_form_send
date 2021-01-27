8d6c3b656e91bd2ada7467b874d98d580f9827df
mena-hub <mssz.nnia@gmail.com>
2021-01-27 13:07:04 -0300

    Application labels aren't unique, duplicates: form

--- web_personal/web_personal/settings.py
+++ web_personal/web_personal/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'form.apps.FormConfig',
     'bootstrap4',
     'core',
     'form',
